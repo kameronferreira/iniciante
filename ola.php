25d80d4c26466cee3548200a240ba61be3ba180a
kameronferreira <59830861+kameronferreira@users.noreply.github.com>
2020-01-22 13:22:50 -0300

    ola
    teste

---
+++ ola.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+ <title> Primeiro Exemplo PHP</title>
+</head>
+<body>
+<h1>Primeiro Exemplo PHP </h1>
+</body>
+<?php
+echo "olá, Mundo!";
+
+?>
+
+</html>
